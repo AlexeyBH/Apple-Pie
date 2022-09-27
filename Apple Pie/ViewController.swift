243e04811ff98c683dffcd2d042fb86df87e671d
AlexeyBH <alexey_bh@mail.ru>
2022-09-27 17:57:45 +0300

    Array added

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet var apples: [UIImageView]!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    @IBOutlet var correctWord: NSLayoutConstraint!
+ 
+    @IBOutlet var scoreLabel: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
